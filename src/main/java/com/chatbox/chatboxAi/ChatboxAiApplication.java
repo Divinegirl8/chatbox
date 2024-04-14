9e2c525c6a0ad359c82c849dfc35d91dc206a496
James DivineMercy <iamoluchimercy@gmail.com>
2024-04-14 17:32:38 +0100

    first commit

---
+++ src/main/java/com/chatbox/chatboxAi/ChatboxAiApplication.java
@@ -0,0 +1,13 @@
+package com.chatbox.chatboxAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatboxAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatboxAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatbox/chatboxAi/ChatboxAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatbox.chatboxAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatboxAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
